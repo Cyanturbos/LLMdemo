7963037b9fc830411ab01141d0ac11ac7b67a52f
Cyanturbos <cyanturbo@163.com>
2024-08-19 13:47:45 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from openai import AsyncOpenAI
+import chainlit as cl
+client = AsyncOpenAI()
+
+# Instrument the OpenAI client
+cl.instrument_openai()
+
+settings = {
+    "model": "gpt-3.5-turbo",
+    "temperature": 0,
+    # ... more settings
+}
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    response = await client.chat.completions.create(
+        messages=[
+            {
+                "content": "You are a helpful bot, you always reply in Spanish",
+                "role": "system"
+            },
+            {
+                "content": message.content,
+                "role": "user"
+            }
+        ],
+        **settings
+    )
+    await cl.Message(content=response.choices[0].message.content).send()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
